a3e37d5e817d0f22472f16501750a93d3783fad6
filip <djordjevic_f@icloud.com>
2026-03-05 18:51:30 +0100

    multiplayer sem naredu

--- pygamkaca.py
+++ pygamkaca.py
@@ -34,27 +34,37 @@ modra = (0,0,255)
 
 clock = pygame.time.Clock()
 smer = "desno"
+smer2 = "levo"
 speed = 50
 run = True
+jabb = [50,100,150,200,250,300,350,400,450]
 kvad1 = pygame.Rect(0,0,50,50)
 kvad2 = pygame.Rect(0,0,50,50)
-kvad3 = pygame.Rect(random.randint(0,500),random.randint(0,500),50,50)
+kvad3 = pygame.Rect(random.choice(jabb),random.choice(jabb),50,50)
+kvad4 = pygame.Rect(200,300,50,50)
+
 lenght = 3
 prejsnji = []
 koordkac = []
+lenght2 = 3
+prejsnji2 = []
+koordkac2 = []
 
 
 obratzanke = 0
 font = pygame.font.Font('freesansbold.ttf', 32)
-text2 = font.render("score: " + str(lenght+1), True, črna)
+text2 = font.render("score: " + str(lenght+1), True, črna, (255,255,255))
+text2.set_alpha(127)
 textRect2 = text2.get_rect()
 textRect2.center = (85, 50)
-
 
 font = pygame.font.Font('freesansbold.ttf', 32)
 text = font.render('KONEC!!!!!!!      D:', True, rdeča, bela)
 textRect = text.get_rect()
 textRect.center = (250, 250)
+
+
+
 def konc():
     run=False
     ekran.blit(text,textRect)
@@ -68,14 +78,26 @@ def konc():
 
 
 pygame.draw.rect(ekran,(0,255,0),kvad3)
+barva = zelena
+
 
 while run:
+
+
+
+    for i in koordkac:
+        if 0 < koordkac2.count(i):
+            konc()
+    
+
+    
+    obratzanke += 1
     text2 = font.render("score: " + str(lenght+1), True, črna, bela)
 
 
 
     xy = [kvad1.x,kvad1.y]
-    
+    xy2 = [kvad4.x,kvad4.y]
     
     
     clock.tick(30)
@@ -90,9 +112,13 @@ while run:
     ekran.fill((255,255,255))
     
     
-    pygame.draw.rect(ekran,(0,255,0),kvad1)
+    pygame.draw.rect(ekran,barva,kvad1)
     for i in koordkac:
         if koordkac.count(i) > 1:
+            konc()
+
+    for i in koordkac2:
+        if koordkac2.count(i) > 6:
             konc()
     #premikanje
     pressed = pygame.key.get_pressed()
@@ -104,6 +130,20 @@ while run:
         smer = "levo"
     if pressed[pygame.K_RIGHT]:
         smer = "desno"
+
+    if pressed[pygame.K_SPACE]:
+        barva  = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
+        
+    if pressed[pygame.K_s]:
+        smer2 = "dol"
+    if pressed[pygame.K_w]:
+        smer2 = "gor"
+    if pressed[pygame.K_a]:
+        smer2 = "levo"
+    if pressed[pygame.K_d]:
+        smer2 = "desno"
+
     
     pygame.draw.rect(ekran,(255,0,0),kvad3)
 
@@ -118,16 +158,39 @@ while run:
             kvad1.y = kvad1.y + speed
         prejsnji.append(xy)
         koordkac = prejsnji[-lenght:]
+
+        if smer2 == "desno":
+            kvad4.x = kvad4.x + speed
+            
+        if smer2 == "levo":
+            kvad4.x = kvad4.x - speed
+            
+        if smer2 == "gor":
+            kvad4.y = kvad4.y - speed
+            
+        if smer2 == "dol":
+            kvad4.y = kvad4.y + speed
+            
+        prejsnji2.append([kvad4.x,kvad4.y])
+        koordkac2 = prejsnji2[-lenght2:]
         
 
 
     if kvad1.colliderect(kvad3):
-        kvad3 = pygame.Rect(random.randint(0,500),random.randint(0,500),50,50)
+        kvad3 = pygame.Rect(random.choice(jabb),random.choice(jabb),50,50)
         lenght +=1
+
+    if kvad4.colliderect(kvad3):
+        kvad3 = pygame.Rect(random.choice(jabb),random.choice(jabb),50,50)
+        lenght2 +=1
 
     for i in koordkac:
         kvad2 = pygame.Rect(i[0],i[1],50,50)
-        pygame.draw.rect(ekran,(0,255,0),kvad2)
+        pygame.draw.rect(ekran,barva,kvad2)
+
+    for i in koordkac2:
+        kvad4 = pygame.Rect(i[0],i[1],50,50)
+        pygame.draw.rect(ekran,barva,kvad4)
 
     
     if kvad1.x > 450:
@@ -141,10 +204,24 @@ while run:
     for i in koordkac:
         if koordkac.count(i) > 1:
             konc()
+            
+    if kvad4.x > 450:
+        konc()
+    if kvad4.x < 0:
+        konc()
+    if kvad4.y > 450:
+        konc()
+    if kvad4.y < 0:
+        konc()
+# NE DELLLLLLLLLLALLLLALALLALALALLALALALLALALALLALALLALALALLLLLLLLLLLLLLLL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! takoj neha
+      
+    for i in koordkac2:
+        if koordkac2.count(i) > 6:
+            konc()
 
 
         
-    obratzanke += 1
+  
     if obratzanke == 31:
         obratzanke = 0
 
---
+++ test.py
@@ -0,0 +1,6 @@
+l1 = [10, 20, 30, 40, 50]
+l2 = [40, 50, 60, 70, 80]
+
+common_elements = set(l1) & set(l2)
+
+print(f"Common elements: {common_elements}")
